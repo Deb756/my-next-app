da7f78d554912461f9667efc405df56c68e96487
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-03-07 07:19:28 +0530

    Routes Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <a href="/users">Users Page</a> <br />
+      <a href="/users/new">New Users Page</a>
+    </>
   );
 }
---
+++ src/app/users/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserPage = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+      Welcome to UserPage
+    </div>
+  )
+}
+
+export default UserPage

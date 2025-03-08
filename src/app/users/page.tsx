c011b80ef67034fb9d0413ef352890db9437e032
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-03-08 14:28:10 +0530

    daisyUi Added

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -13,18 +13,109 @@ const UserPage = async () => {
   });
   const user: User[] = await res.json();
   return (
-    <div className="min-h-screen bg-cyan-950 text-white flex flex-col">
-      <h2 className="text-center p-7 text-3xl">Welcome to UserPage</h2>
-      <h2 className="text-center bg-cyan-800 p-3 m-3 text-3xl">Users</h2>
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+              <div className="indicator">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                <span className="badge badge-sm indicator-item">8</span>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
+              <div className="card-body">
+                <span className="text-lg font-bold">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">View cart</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+        }}>
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div className="min-h-screen bg-cyan-950 text-white flex flex-col">
+        <h2 className="text-center p-7 text-3xl">Welcome to UserPage</h2>
+        <h2 className="text-center bg-cyan-800 p-3 m-3 text-3xl">Users</h2>
 
-      <ul className="flex flex-wrap gap-2.5 items-center justify-center">
-        {user.map((user) => (
-          <li key={user.id} className="border border-white p-5 rounded-md hover:bg-cyan-800  animate-pulse">
-            {user.name}
-          </li>
-        ))}
-      </ul>
-    </div>
+        <ul className="flex flex-wrap p-3 gap-2.5 items-center justify-center">
+          {user.map((user) => (
+            <li key={user.id} className="border border-white p-5 rounded-md hover:bg-cyan-800  animate-pulse">
+              {user.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+              <fieldset className="fieldset">
+                <label className="fieldset-label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+                <label className="fieldset-label">Password</label>
+                <input type="password" className="input" placeholder="Password" />
+                <div><a className="link link-hover">Forgot password?</a></div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
